0c2e08f0e22e102dfa6f255678a10a6fec8d50a3
Tuğçe Ergün <tugceergun000@gmail.com>
2023-12-20 15:11:37 +0300

    Home update

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -4,16 +4,29 @@ import background from "../../assets/background1.png"
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <ImageBackground 
        source={background}
        style={styles.container}>
         <View style={styles.tempWrapper}>
-          <Text style={styles.text}>75</Text>
+          <Text style={styles.text}>75°</Text>
         </View>
+        <View style={styles.data}>
+          <View style={styles.spacer}></View>
+          <View style={styles.dataWrapper}>
+            <View style={styles.humid}>
+              <Text styles={styles.dataText}>23%</Text>
+              <Text style={styles.title}>Humidity</Text>
+            </View>
+            <View style={styles.pressure}>
+              <Text styles={styles.dataText}>22</Text>
+              <Text style={styles.title}>Pressure</Text>
+            </View>
+          </View>
+        </View>
+        <Button title="Log Out" onPress={() => navigation.navigate("Login")} />
       </ImageBackground>
-      <Button title="Log Out" onPress={() => navigation.navigate("Login")} />
-    </View>
+      
+   
   );
 };
 
@@ -32,7 +45,52 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 150,
     fontWeight: "100",
-    textAlign: "center",
+    textAlign: "right",
     color:"white",
+    paddingRight: 35,
+  },
+  data: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%", 
+  },
+  spacer: {
+    height: "30%",
+  },
+  dataWrapper: {
+    backgroundColor: "rgba(255, 255, 255, 0.2)",
+    flexDirection:"row",
+    height: "20%",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "80%",
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "white",
+  },
+  humid: {
+    flex:1,
+    justifyContent: "center",
+    alignItems:"center",
+  },
+  pressure: {
+    flex:1,
+    justifyContent: "center",
+    alignItems:"center",
+  },
+  dataText: {
+    fontSize: 20,
+    fontWeight: "200",
+    color: "white",
+    textAlign: "center",
+    fontFamily: "Helvetica",
+  },
+  title: {
+    fontSize: 12,
+    fontWeight: "bold",
+    color:"white",
+    textAlign: "center",
+    fontFamily: "Helvetica",
   },
 });
