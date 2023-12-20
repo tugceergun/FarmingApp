e326e0f9f90133a4f2892233722d88ee827ef02b
Tuğçe Ergün <tugceergun000@gmail.com>
2023-12-20 15:11:51 +0300

    Firebase update

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import {getDatabase, ref, onVlue} from "firebase/database"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +16,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+const db = getDatabase();
+export { db, ref, onVlue };
