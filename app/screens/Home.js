4ded69ad3c41329cd6ee2590481915c0679c4698
Tuğçe Ergün <tugceergun000@gmail.com>
2023-12-20 14:23:40 +0300

    Home ekrani duzenleme.

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,13 +1,38 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, ImageBackground, StyleSheet } from "react-native";
+import background from "../../assets/background1.png"
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+      <ImageBackground 
+       source={background}
+       style={styles.container}>
+        <View style={styles.tempWrapper}>
+          <Text style={styles.text}>75</Text>
+        </View>
+      </ImageBackground>
       <Button title="Log Out" onPress={() => navigation.navigate("Login")} />
     </View>
   );
-}
+};
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent:"center",
+  },
+  tempWrapper: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 150,
+    fontWeight: "100",
+    textAlign: "center",
+    color:"white",
+  },
+});
